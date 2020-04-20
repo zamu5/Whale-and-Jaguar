0b233513ee5dbd73c2d0d698ad1549ef39f4ee93
Sergio Zamudio <szamu5@hotmail.com>
2020-04-19 23:08:15 -0500

    FEATURE: Implement text analysis create and url that returns the result of the endpoints sentiment, classification, entities, concepts and summarize. Those results are from a specific url wrote in the code

--- apps/analysis/views.py
+++ apps/analysis/views.py
@@ -1,6 +1,16 @@
+from aylienapiclient import textapi
 from django.shortcuts import render
+from django.core.serializers import serialize
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'home.html')
+    endpoints = ['Sentiment', 'Classify', 'Entities', 'Concepts', 'Summarize']
+    url = 'https://techcrunch.com/2019/12/22/who-will-the-winners-be-in-the-future-of-fintech/'
+    data = {}
+    client = textapi.Client('c0fcb430', '749f0eb9482c31ea51fb1b92a028231a')
+    for endpoint in endpoints:
+        command = 'client.' + endpoint + "({'url': url})"
+        data[endpoint] = (eval(command))
+    context = {'data': data}
+    return render(request, 'home.html', context=context)
