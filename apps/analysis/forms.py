b73097c12dd2d011b26ed9e4900b17cf825caa4b
Sergio Zamudio <szamu5@hotmail.com>
2020-04-22 01:09:32 -0500

    FEATURE: Organize the url analyzed in a table New: update the style of the page

--- apps/analysis/forms.py
+++ apps/analysis/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 
+visualization_type = {
+    ('json', 'Json'),
+    ('txt-file', 'Txt file'),
+    ('table', 'Table'),
+}
+
 
 class URL(forms.Form):
     url = forms.CharField(label='URL', max_length=100)
+    type = forms.MultipleChoiceField(required=True, choices=visualization_type)
+
--- apps/analysis/views.py
+++ apps/analysis/views.py
@@ -1,7 +1,9 @@
+from aylienapiclient.errors import HttpError
+from django.http import HttpResponse
 from aylienapiclient import textapi
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
 from .forms import URL
+import json
 
 
 def home(request):
@@ -12,12 +14,23 @@ def home(request):
             url = form.cleaned_data['url']
             data = {}
             client = textapi.Client('c0fcb430', '749f0eb9482c31ea51fb1b92a028231a')
-            for endpoint in endpoints:
-                command = 'client.' + endpoint + "({'url': url})"
-                data[endpoint] = (eval(command))
-            context = {'data': data}
-            return render(request, 'detail.html', context=context)
+            try:
+                for endpoint in endpoints:
+                    command = 'client.' + endpoint + "({'url': url})"
+                    data[endpoint] = (eval(command))
+                context = {'data': data}
+                dump = json.dumps(context)
+                if form.cleaned_data['type'][0] == 'json':
+                    return HttpResponse(dump, content_type='application/json')
+                elif form.cleaned_data['type'][0] == 'table':
+                    context['form'] = URL()
+                    return render(request, 'detail.html', context=context)
+                else:
+                    return HttpResponse(dump, content_type='text/x-json')
+            except HttpError:
+                return render(request, 'error.html', {'form': URL()})
     else:
         form = URL()
     return render(request, 'home.html', {'form': form})
+
 
